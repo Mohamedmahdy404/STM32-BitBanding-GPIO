6ef81a2c80a4712ca8250456d8517716cd475808
Mohamedmahdy404 <166864037+Mohamedmahdy404@users.noreply.github.com>
2024-04-12 21:31:55 +0200

    Update main.c

--- BitBanding-GPIO/src/main.c
+++ BitBanding-GPIO/src/main.c
@@ -1,41 +1,56 @@
-/*
- * main.c
- *
- *  Created on: Sep 21, 2024
- *
- */
-
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-
 #include "DIO_interface.h"
 #include "STK_interface.h"
 #include "RCC_interface.h"
 
-
-
-
-void main(void)
+// Function to initialize system
+void System_init(void)
 {
-	RCC_voidInitSysClock();
-		RCC_voidEnableClock(RCC_APB2,2);   //gpioa
-		RCC_voidEnableClock(RCC_APB2, 14);
-		RCC_voidEnableClock(RCC_APB2,12);
-		MSTK_voidInit();
+    // Initialize system clock
+    RCC_voidInitSysClock();
 
-			MGPIO_VidSetPinDirection(GPIOA,1,0b0010);
-			MGPIO_VidSetPinDirection(GPIOA,5,0b0010);
-			MGPIO_VidSetPinDirection(GPIOA,10,0b0010);
+    // Enable clocks for GPIOA and other peripherals
+    RCC_voidEnableClock(RCC_APB2, 2);   // GPIOA
+    RCC_voidEnableClock(RCC_APB2, 14);
+    RCC_voidEnableClock(RCC_APB2, 12);
 
-			MGPIO_VidSetPinValue(GPIOA,1,LOW);
-			MGPIO_VidSetPinValue(GPIOA,5,HIGH);
-
-
-			u32 var = 0;
-			 SET_BIT_SRAM(var, 1);
-			 MGPIO_VidSetPinValue(GPIOA,10,GET_BIT_SRAM(var, 1));
-
-
-	while(1);
+    // Initialize systick
+    MSTK_voidInit();
 }
 
+// Function to setup GPIO
+void GPIOSetup(void)
+{
+    // Set pin directions
+    MGPIO_VidSetPinDirection(GPIOA, 1, 0b0010);
+    MGPIO_VidSetPinDirection(GPIOA, 5, 0b0010);
+    MGPIO_VidSetPinDirection(GPIOA, 10, 0b0010);
+
+    // Set pin values
+    MGPIO_VidSetPinValue(GPIOA, 1, LOW);
+    MGPIO_VidSetPinValue(GPIOA, 5, HIGH);
+}
+
+// Main function
+int main(void)
+{
+    // Initialize system
+    System_init();
+
+    // Setup GPIO
+    GPIOSetup();
+
+    // Variable for bit-banding
+    u32 var = 0;
+
+    // Set bit 1 of var
+    SET_BIT_SRAM(var, 1);
+
+    // Set GPIOA pin 10 value to bit 1 of var
+    MGPIO_VidSetPinValue(GPIOA, 10, GET_BIT_SRAM(var, 1));
+
+    // Infinite loop
+    while(1);
+
+}
